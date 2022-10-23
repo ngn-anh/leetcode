1ce25ad5d2b2992f6d0433a4162ec70d9f903579
Ngân <102570839+ngn-anh@users.noreply.github.com>
2022-10-23 19:56:50 +0700

    Add files via upload

---
+++ 338. Counting Bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+       vector<int> res(n+1,0);
+        for ( int i = 0; i <= n; i++)
+        {
+            if ( i % 2 == 0)
+            {
+                res[i] = res[i/2];
+            }
+            else
+            {
+                res[i] = res[i/2] + 1;
+            }
+        }
+        return res;
+    }
+    
+};
